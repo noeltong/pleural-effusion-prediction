c2b1719d5312432f9eecb3d112626cc5f00d38fd
noeltong <2441624985@qq.com>
2023-04-12 14:32:55 +0800

    update config

--- configs/default_config.py
+++ configs/default_config.py
@@ -10,10 +10,10 @@ def get_config():
     # ----------------
 
     training = cfg.training = ConfigDict()
-    training.num_epochs = 100
-    training.batch_size = 64
-    training.save_ckpt_freq = 10
-    training.eval_freq = 10
+    training.num_epochs = 10000
+    training.batch_size = 32
+    training.save_ckpt_freq = 100
+    training.eval_freq = 5
 
     # ----------------
     # Model
@@ -21,9 +21,10 @@ def get_config():
 
     model = cfg.model = ConfigDict()
     model.clip_grad_norm = 1.
-    model.ema = True
-    model.ema_rate = 0.999
+    model.ema = False
+    model.ema_rate = 0.99
     model.ema_steps = 1
+    model.depths = [2, 2, 2, 2]
 
     # ----------------
     # Optimization
@@ -33,18 +34,19 @@ def get_config():
     optim.optimizer = 'AdamW'
     optim.schedule = 'CosineAnnealingLR'
     optim.grad_clip = 1.
-    optim.initial_lr = 0.0005
+    optim.initial_lr = 0.000075
     optim.weight_decay = 0.0001
     optim.min_lr = 0.001 * optim.initial_lr
-    optim.warmup_epochs = None
+    optim.warmup_epochs = 250
 
     # ----------------
     # Data
     # ----------------
 
     cfg.data = data = ConfigDict()
+    data.path = '/root/pred-pe/data'
     data.num_workers = 2
-    data.prefetch_factor = 1
+    data.prefetch_factor = 2
 
     cfg.seed = 42
     cfg.distributed = True
