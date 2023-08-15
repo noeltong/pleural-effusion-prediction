61b74d22534bc87620e52a18feef959a75746a85
noeltong <2441624985@qq.com>
2023-08-15 14:48:01 +0800

    update

---
+++ scripts/pretrain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=edm-train
+#SBATCH --partition=normal
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:4
+#SBATCH --mail-type=all
+#SBATCH --mail-user=tongshq@shanghaitech.edu.cn
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+#SBATCH --time=10-00:00:00
+#SBATCH --nodelist=sist_gpu49
+
+torchrun --nproc_per_node=4 main.py --mode train --config /public/home/tongshq/pleural-effusion-prediction/configs/pretrain.py --workdir pertrain_vit_b
